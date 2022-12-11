aecc863e026e0f619e2876a55628a3ad937847c2
alehey <alexey.p.svyz@mail.ru>
2022-12-11 16:46:44 +0700

    add semtask3

---
+++ semtask3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    int result = number1 / number2;
+    Console.WriteLine("Число кратно, ответ " + result);
+}
+else
+{
+    int result = number1 % number2;
+    Console.WriteLine("Число некратно, остаток " + result);
+}
