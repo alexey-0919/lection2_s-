ade624b6fdfecfe065ccab37e3062734873cf633
alehey <alexey.p.svyz@mail.ru>
2022-12-11 16:33:24 +0700

    add semtask2

---
+++ semtask2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Random random = new Random();
+//int number = random.Next(100, 1000);
+//Console.WriteLine($"Random number = {number}");
+
+//int a = number / 100;
+//int c = number % 10;
+
+//Console.WriteLine("Ответ " +a +c);
+
+
+Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine($"Random number = {number}");
+int delete = 2;
+var str = number.ToString();
+number = int.Parse(str.Remove(str.Length - delete, 1));
+Console.WriteLine(number);
+
+//str.remove(1, 1) позиция / количество 369 (6 удаляется)
